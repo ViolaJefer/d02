b06ff27fcfaf2b8635f2abfa5dda8bb04487345e
ViolaJefer <violajeferwow@gmail.com>
2018-02-12 00:52:07 +0300

    alphabet

---
+++ src/d02.java
@@ -0,0 +1,12 @@
+public class d02 {
+
+    public static void main(String[] args) {
+
+        ft_print_alphabet();
+    }
+
+    public static void ft_print_alphabet() {
+
+        System.out.println("abcdefghijklmnopqrstuvwxyz");
+    }
+}
